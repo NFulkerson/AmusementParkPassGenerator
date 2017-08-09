6d21438705d7984531e1e16b84110ca14267ac19
Nathan <nathan@macui-MacBook-Pro.local>
2017-08-09 13:33:24 +0900

    Initial Commit

---
+++ GuestPassesSB/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GuestPassesSB
+//
+//  Created by Nathan on 8/9/17.
+//  Copyright © 2017 Nathan Fulkerson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
