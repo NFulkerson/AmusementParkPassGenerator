bc6eae91960eac0399919702e5055268ba2182ae
Nathan <nathan@macui-MacBook-Pro.local>
2017-08-20 17:37:46 +0900

    Finalizing layout of form view

---
+++ GuestPassesSB/MenuBarButton.swift
@@ -0,0 +1,13 @@
+//
+//  MenuBarButton.swift
+//  GuestPassesSB
+//
+//  Created by Nathan on 8/16/17.
+//  Copyright © 2017 Nathan Fulkerson. All rights reserved.
+//
+
+import UIKit
+
+class MenuBarButton: UIButton {
+
+}
--- GuestPassesSB/ViewController.swift
+++ GuestPassesSB/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var mainMenuUIButtons: [UIButton]!
+    @IBOutlet var subMenuUIButtons: [UIButton]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
