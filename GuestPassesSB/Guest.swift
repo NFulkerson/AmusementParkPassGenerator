4a558fa0d3261d68a09d0cd1726fb97ef658f518
Nathan <nathan@macui-MacBook-Pro.local>
2017-10-07 16:13:49 +0900

    Merge child guest pass type with general guest type.

--- GuestPassesSB/Guest.swift
+++ GuestPassesSB/Guest.swift
@@ -9,16 +9,23 @@
 import Foundation
 
 struct Guest: Entrant, RideAccessible, DiscountQualifiable {
+    var firstName: String?
+    var middleName: String?
+    var lastName: String?
+    var birthDate: Date?
     var type: GuestType
+    
     enum GuestType {
         case classic
         case vip
         case senior
         case seasonPass
+        case child
     }
+    
     var discounts: (food: PercentDiscount, merch: PercentDiscount) {
         switch self.type {
-        case .classic:
+        case .classic, .child:
             return (food: 0.0, merch: 0.0)
         case .vip:
             return (food: 0.10, merch: 0.20)
@@ -28,25 +35,30 @@ struct Guest: Entrant, RideAccessible, DiscountQualifiable {
             return (food: 0.10, merch: 0.20)
         }
     }
-}
-
-struct Senior: Identifiable, LineSkippable {
-    var firstName: String?
-    var middleName: String?
-    var lastName: String?
-    var birthDate: Date
     
-    init(birthDate: String) throws {
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "MM/dd/yyyy"
-        
-        guard let bday = dateFormatter.date(from: birthDate) else {
-            throw DOBError.dateConversionError("Couldn't create date from string.")
-        }
-        self.birthDate = bday
-        
-        if bday.age < 60 {
-            throw DOBError.dobInvalid("Guest doesn't yet qualify for senior admission.")
+    init(guestType: GuestType, birthDate: String? = nil) throws {
+        switch guestType {
+        case .child, .senior:
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateFormat = "MM/dd/yyyy"
+            
+            guard let dob = birthDate ,let bday = dateFormatter.date(from: dob) else {
+                throw DOBError.dateConversionError("Couldn't create date from string.")
+            }
+            if guestType == .senior && bday.age < 60 {
+                throw DOBError.dobInvalid("Guest doesn't yet qualify for senior admission.")
+            } else if guestType == .child && bday.age > 5 {
+                throw DOBError.dobInvalid("Child is too old to qualify for free admission.")
+            } else if guestType == .child && bday.age < 0 {
+                throw DOBError.dobInvalid("We don't need passes for children not born yet.")
+            }
+            
+            self.birthDate = bday
+            self.type = guestType
+        default:
+            self.type = guestType
         }
     }
+    
 }
+
